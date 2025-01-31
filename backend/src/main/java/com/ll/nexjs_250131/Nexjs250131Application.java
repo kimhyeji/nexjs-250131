ac20709aeaf95f648af630b76d1c0e9e1966536e
kimhyeji <gpwl5589@gmail.com>
2025-01-31 18:36:36 +0900

    test init

---
+++ backend/src/main/java/com/ll/nexjs_250131/Nexjs250131Application.java
@@ -0,0 +1,13 @@
+package com.ll.nexjs_250131;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Nexjs250131Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Nexjs250131Application.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/ll/nexjs_250131/Nexjs250131ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ll.nexjs_250131;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Nexjs250131ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
